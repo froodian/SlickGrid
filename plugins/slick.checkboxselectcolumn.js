da85abfac6ea5f47a7df521e1b4a2cc2d0ce26d0
Ian Leue <ian@appboy.com>
2014-12-30 12:40:31 -0500

    My rewrite of checkboxselectcolumn

--- plugins/slick.checkboxselectcolumn.js
+++ plugins/slick.checkboxselectcolumn.js
@@ -1,3 +1,20 @@
+/***
+ * This plugin has been MODIFIED BY APPBOY to support some SIGNIFICANT BEHAVIORAL DIFFERENCES from the original example
+ * at http://mleibman.github.io/SlickGrid/examples/example-checkbox-row-select.html
+ *
+ *   * Clicking an unchecked header causes only the visible rows (instead of all rows) to be selected.  This is
+ *       to support remotemodel, where we don't have all rows at all times.
+ *   * When some rows are selected, the checkbox will appear indeterminate (like [-]).  When all visible rows are
+ *       selected, the underlying checkbox will be checked (so that clicking it will uncheck all rows), and when some
+ *       of the visible rows are selected, the underlying checkbox will be unchecked (so that clicking it will cause all
+ *       formerly selected rows and all visible rows to be selected).
+ *   * This plugin no longer automatically re-renders the grid when selected rows change.  Subscribe to
+ *       onSelectedRowsChanged and render manually if you want.  See "Ian commented out this line" for more details.
+ *   * Changed header selection/deselection to not invalidate each row, but to simply mark each row selected/deselected
+ *       and to manually set the state on that row's checkbox.  This prevents the entire row from needing to rerender.
+ *   * Made entire checkbox cell clickable
+ *   * Support programmatic calling of setSelectedRows on the grid
+ */
 (function ($) {
   // register namespace
   $.extend(true, window, {
@@ -9,6 +26,7 @@
 
   function CheckboxSelectColumn(options) {
     var _grid;
+    var _$headerCell = null;
     var _self = this;
     var _handler = new Slick.EventHandler();
     var _selectedRowsLookup = {};
@@ -27,7 +45,10 @@
         .subscribe(_grid.onSelectedRowsChanged, handleSelectedRowsChanged)
         .subscribe(_grid.onClick, handleClick)
         .subscribe(_grid.onHeaderClick, handleHeaderClick)
+        .subscribe(_grid.onHeaderCellRendered, handleHeaderCellRendered)
+        .subscribe(_grid.onViewportChanged, handleViewportChanged)
         .subscribe(_grid.onKeyDown, handleKeyDown);
+      _$headerCell = null;
     }
 
     function destroy() {
@@ -41,20 +62,48 @@
         row = selectedRows[i];
         lookup[row] = true;
         if (lookup[row] !== _selectedRowsLookup[row]) {
-          _grid.invalidateRow(row);
           delete _selectedRowsLookup[row];
         }
       }
-      for (i in _selectedRowsLookup) {
-        _grid.invalidateRow(i);
-      }
       _selectedRowsLookup = lookup;
-      _grid.render();
+      applyFunctionToVisibleRows(function(i) {
+        // Find this row's checkbox and set its state depending on whether it should be selected or not
+        setRowCheckbox(i, _grid.getColumnIndex(_options.columnId), !!_selectedRowsLookup[i]);
+      });
+      setHeaderCellState();
+    }
 
-      if (selectedRows.length && selectedRows.length == _grid.getDataLength()) {
-        _grid.updateColumnHeader(_options.columnId, "<input type='checkbox' checked='checked'>", _options.toolTip);
-      } else {
-        _grid.updateColumnHeader(_options.columnId, "<input type='checkbox'>", _options.toolTip);
+    function handleViewportChanged(e, args) {
+      applyFunctionToVisibleRows(function(i) {
+        // Find this row's checkbox and set its state depending on whether it should be selected or not
+        setRowCheckbox(i, _grid.getColumnIndex(_options.columnId), !!_selectedRowsLookup[i]);
+      });
+      setHeaderCellState();
+    }
+
+    function setHeaderCellState() {
+      if (_$headerCell != null) {
+        // Ian commented out this line, because the grid subscriptions should handle rerendering as necessary when
+        // rows have been invalidated.  In a remotemodel grid, this render call would cause all data to disappear
+        // until it had been reloaded.
+        //_grid.render();
+
+        var viewport = _grid.getViewport();
+        var allVisibleRowsAreChecked = true;
+        for (var i = viewport.top; i < Math.min(viewport.bottom, _grid.getDataLength()); i++) {
+          if (_selectedRowsLookup[i] == null || !_selectedRowsLookup[i]) {
+            allVisibleRowsAreChecked = false;
+          }
+        }
+
+        var selectedRows = _grid.getSelectedRows();
+        var someRowsAreSelected = selectedRows.length && selectedRows.length > 0;
+
+        // We want the header to be checked if all visible rows are selected
+        _$headerCell.prop('checked', someRowsAreSelected && allVisibleRowsAreChecked);
+
+        // We want the header to be indeterminate if only some rows are selected
+        _$headerCell.prop("indeterminate", someRowsAreSelected && selectedRows.length < _grid.getDataLength());
       }
     }
 
@@ -73,7 +122,7 @@
 
     function handleClick(e, args) {
       // clicking on a row select checkbox
-      if (_grid.getColumns()[args.cell].id === _options.columnId && $(e.target).is(":checkbox")) {
+      if (_grid.getColumns()[args.cell].id === _options.columnId) {
         // if editing, try to commit
         if (_grid.getEditorLock().isActive() && !_grid.getEditorLock().commitCurrentEdit()) {
           e.preventDefault();
@@ -81,14 +130,18 @@
           return;
         }
 
-        toggleRowSelection(args.row);
+        toggleRowSelection(args.row, args.cell);
         e.stopPropagation();
         e.stopImmediatePropagation();
       }
     }
 
-    function toggleRowSelection(row) {
-      if (_selectedRowsLookup[row]) {
+    function toggleRowSelection(row, cell) {
+      var alreadySelected = !!_selectedRowsLookup[row];
+
+      setRowCheckbox(row, cell, !alreadySelected);
+
+      if (alreadySelected) {
         _grid.setSelectedRows($.grep(_grid.getSelectedRows(), function (n) {
           return n != row
         }));
@@ -106,17 +159,41 @@
           return;
         }
 
-        if ($(e.target).is(":checked")) {
-          var rows = [];
-          for (var i = 0; i < _grid.getDataLength(); i++) {
-            rows.push(i);
-          }
-          _grid.setSelectedRows(rows);
+        var selecting = $(e.target).is(":checked");
+        var rowsToSelect = _grid.getSelectedRows();
+        applyFunctionToVisibleRows(function(i) {rowsToSelect.push(i);});
+
+        if (selecting) {
+          _grid.setSelectedRows(rowsToSelect);
         } else {
           _grid.setSelectedRows([]);
         }
         e.stopPropagation();
         e.stopImmediatePropagation();
+      }
+    }
+
+    // Applies the given function to each visible row.
+    // *func (Function) - Takes the row index
+    function applyFunctionToVisibleRows(func) {
+      var viewport = _grid.getViewport();
+      // To support use with slick.remotemodel, only operate on rows which are visible in the viewport and for which
+      // we actually have data
+      for (var i = viewport.top; i < Math.min(viewport.bottom, _grid.getDataLength()); i++) {
+        var dataItem = _grid.getDataItem(i);
+        if (dataItem != null) {
+          func(i);
+        }
+      }
+    }
+
+    function setRowCheckbox(row, cell, checked) {
+      $(_grid.getCellNode(row, cell)).find("input[type=checkbox]").prop("checked", checked);
+    }
+
+    function handleHeaderCellRendered(e, args) {
+      if (args.column.id == _options.columnId) {
+        _$headerCell = $(args.node).find("input[type=checkbox]");
       }
     }
 
